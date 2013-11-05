c51acaf408aaa776ac06b13bd44bfe76a910dd9d
junaos <junaos@li614-167.members.linode.com>
2013-11-05 03:27:11 +0000

    Started Front-end work

--- frontend/hosting/views.py
+++ frontend/hosting/views.py
@@ -1,1 +1,11 @@
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponseRedirect, HttpResponse
+from django.core.urlresolvers import reverse
+from django.views import generic
+
+from hosting.models import MinecraftPlan, MinecraftFeature
+
 # Create your views here.
+def index(request):
+
+    return HttpResponse("Hello, world.  You're at the index.")
